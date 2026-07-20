bf259d21dcfb43937b1f5deeef2a5070b7242b54
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-07-20 07:04:46 -0500

    feat(flake): register rust dev shell template

--- flake.nix
+++ flake.nix
@@ -34,6 +34,11 @@
       description = "ruby on rails dev shell with direnv";
     };
 
+    templates.rust = {
+      path = ./templates/rust;
+      description = "rust dev shell with direnv";
+    };
+
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
---
+++ templates/rust/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "anirudh's rust dev environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # rust
+          cargo
+          clippy
+          rust-analyzer
+          rustc
+          rustfmt
+
+          # "accessories" for rust
+          bacon
+        ];
+      };
+    };
+}
