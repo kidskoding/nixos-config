b4401d2601c9131cc6231343654837d3d702fc93
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-08-02 07:17:28 -0500

    refactor(emacs): drop emacs daemon service

--- home-manager/modules/emacs/default.nix
+++ home-manager/modules/emacs/default.nix
@@ -1,13 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
+
 {
   home.sessionVariables = {
     EDITOR = ''emacsclient -nw -a ""'';
     VISUAL = ''emacsclient -nw -a ""'';
-  };
-
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs-unstable-nox;
   };
 
   xdg.configFile."doom" = {
