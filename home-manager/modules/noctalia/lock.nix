3bd9ca3af3ff1ce6db8c401d04a8cbfce89a812d
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-08-29 18:01:00 -0500

    patched the noctalia lock screen panel layout and power button colors

--- home-manager/modules/noctalia/lock.nix
+++ home-manager/modules/noctalia/lock.nix
@@ -1,6 +1,17 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 
 {
+  programs.noctalia-shell.package =
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
+      postPatch = (old.postPatch or "") + ''
+        substituteInPlace Modules/LockScreen/LockScreenPanel.qml \
+          --replace-fail $'backgroundColor: Color.mError' $'backgroundColor: Color.mError\n            hoverColor: Color.mError\n            textHoverColor: Color.mOnError' \
+          --replace-fail $'Layout.fillWidth: batteryIndicator.isReady' $'Layout.fillWidth: true' \
+          --replace-fail $'Layout.alignment: (batteryIndicator.isReady) ? (Qt.AlignRight | Qt.AlignVCenter) : Qt.AlignVCenter' $'Layout.alignment: Qt.AlignHCenter | Qt.AlignVCenter' \
+          --replace-fail $'        Item {\n          Layout.preferredWidth: Style.marginM\n        }\n      }\n\n      // Password input' $'        Item {\n          Layout.fillWidth: true\n        }\n      }\n\n      // Password input'
+      '';
+    });
+
   programs.noctalia-shell.settings.idle = {
     enabled = true;
     lockTimeout = 300;
