71c1eddf2e2287a8c3f086baa488fe30e33df45e
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-06 15:09:54 -0500

    switched the doom theme to gruvbox dark medium

--- home-manager/emacs/doom.d/config.el
+++ home-manager/emacs/doom.d/config.el
@@ -1,7 +1,7 @@
 (setq user-full-name "Anirudh Konidala"
       user-mail-address "anirudhkonidala@gmail.com")
 
-(setq doom-theme 'gruvbox-dark-hard)
+(setq doom-theme 'gruvbox-dark-medium)
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
 
