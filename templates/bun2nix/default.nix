6c071199161d53814d1d2b7c427f653c8f981bb3
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-06 15:09:20 -0500

    fixed the bun2nix template so nix run works on a fresh bun init

--- templates/bun2nix/default.nix
+++ templates/bun2nix/default.nix
@@ -1,7 +1,11 @@
 { bun2nix, ... }:
 
+let
+  packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+in
 bun2nix.mkDerivation {
   packageJson = ./package.json;
+  version = packageJson.version or "0.1.0";
 
   src = ./.;
 
--- templates/bun2nix/flake.nix
+++ templates/bun2nix/flake.nix
@@ -10,13 +10,6 @@
     };
   };
 
-  nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   outputs = { nixpkgs, bun2nix, ... }:
     let
       system = "x86_64-linux";
@@ -28,7 +21,10 @@
       packages.${system}.default = pkgs.callPackage ./default.nix { };
 
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ bun bun2nix ];
+        packages = with pkgs; [
+          bun
+          bun2nix
+        ];
 
         shellHook = ''
           bun install --frozen-lockfile
