bb7ebf9c63d61016ce6353bf99cfe484c043273a
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-06 22:53:10 -0500

    pinned apps in launcher declaratively

--- home-manager/noctalia/launcher.nix
+++ home-manager/noctalia/launcher.nix
@@ -5,5 +5,23 @@
     position = "center";
     terminalCommand = "alacritty -e";
     enableClipboardHistory = true;
+    sortByMostUsed = false;
+    pinnedApps = [
+      "Alacritty"
+      "Celeste"
+      "discord"
+      "emacsclient"
+      "Enter the Gungeon"
+      "Hollow Knight"
+      "lunarclient"
+      "org.kde.dolphin"
+      "qimgv"
+      "rs.ruffle.Ruffle"
+      "spotify"
+      "Stardew Valley"
+      "steam"
+      "The Binding of Isaac Rebirth"
+      "thunderbird"
+    ];
   };
 }
