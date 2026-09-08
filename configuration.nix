89100b95f90db8d04657daf602fafffefaf0f781
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-07 20:00:04 -0500

    added devenv cachix substituter

--- configuration.nix
+++ configuration.nix
@@ -19,10 +19,16 @@
   nix.settings.warn-dirty = false;
 
   # prebuilt emacs-overlay/nix-community builds instead of compiling from source
-  nix.settings.substituters = [ "https://nix-community.cachix.org" ];
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+    "https://devenv.cachix.org"
+  ];
   nix.settings.trusted-public-keys = [
     # bun2nix trusted public key
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+    # devenv trusted public key
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
   ];
 
   security.rtkit.enable = true;
