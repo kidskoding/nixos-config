e0b356363a4fffc7849961f8f5b94e0228a2120e
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-14 13:24:50 -0500

    feat(nvidia): switch to open kernel module

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -10,7 +10,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     powerManagement = {
