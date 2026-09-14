49c5b6dce1aac8fbe7d9fbeb10ef2a51cd22d3bb
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-14 13:24:50 -0500

    feat(greeter): set login avatar via accountsservice

--- home-manager/noctalia/greeter.nix
+++ home-manager/noctalia/greeter.nix
@@ -70,5 +70,8 @@ in
     };
   };
 
+  systemd.tmpfiles.settings."10-accountsservice-avatar"."/var/lib/AccountsService/users/anirudh"."f+".argument =
+    "[User]\nIcon=${./avatar.jpg}\n";
+
   fonts.packages = [ pkgs.nerd-fonts.gohufont ];
 }
