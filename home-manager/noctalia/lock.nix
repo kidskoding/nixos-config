e34e4dc513f42af2a53d2e208bb22d1269b2437c
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-14 13:24:50 -0500

    feat(lock): lock session via hypridle before sleep

--- home-manager/noctalia/lock.nix
+++ home-manager/noctalia/lock.nix
@@ -12,6 +12,13 @@
       '';
     });
 
+  services.hypridle = {
+    enable = true;
+    settings.general = {
+      lock_cmd = "noctalia-shell ipc call lockScreen lock";
+      before_sleep_cmd = "noctalia-shell ipc call lockScreen lock";
+    };
+  };
 
   programs.noctalia-shell.settings.idle = {
     enabled = true;
