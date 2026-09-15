09011d34b8543bcac6f28224d191add27fee2111
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-15 09:09:41 -0500

    Setting default shell to bash, but explicit terminal emulators will use my fish config

--- home-manager/emacs/doom.d/config.el
+++ home-manager/emacs/doom.d/config.el
@@ -4,6 +4,10 @@
 (setq doom-theme 'gruvbox-dark-medium)
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
+(setq shell-file-name (executable-find "bash"))
+
+(setq-default vterm-shell "/run/current-system/sw/bin/fish")
+(setq-default explicit-shell-file-name "/run/current-system/sw/bin/fish")
 
 (load! "lisp/ui")
 (load! "lisp/completion")
