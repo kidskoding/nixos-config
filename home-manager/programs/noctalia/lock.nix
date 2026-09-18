545a7b2675d98a66cb254731871f733b70d2bbe0
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-17 22:22:53 -0500

    feat(noctalia): center lock screen info row, always show media, 12h clock

--- home-manager/programs/noctalia/lock.nix
+++ home-manager/programs/noctalia/lock.nix
@@ -4,11 +4,22 @@
   programs.noctalia-shell.package =
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
       postPatch = (old.postPatch or "") + ''
+        substituteInPlace Modules/Panels/Media/MediaPlayerPanel.qml \
+          --replace-fail $'        Layout.preferredHeight: headerRow.implicitHeight + Style.margin2M' $'        visible: false' \
+          --replace-fail $'        Layout.preferredHeight: mediaContentGrid.implicitHeight + Style.margin2M\n' $'        Layout.preferredHeight: mediaContentGrid.implicitHeight + Style.margin2M\n\n        NIconButton {\n          anchors.top: parent.top\n          anchors.right: parent.right\n          anchors.margins: Style.marginS\n          z: 2\n          icon: "close"\n          baseSize: Style.baseWidgetSize * 0.6\n          onClicked: root.close()\n        }\n'
         substituteInPlace Modules/LockScreen/LockScreenPanel.qml \
           --replace-fail $'backgroundColor: Color.mError' $'backgroundColor: Color.mError\n            hoverColor: Color.mError\n            textHoverColor: Color.mOnError' \
-          --replace-fail $'Layout.fillWidth: batteryIndicator.isReady' $'Layout.fillWidth: true' \
-          --replace-fail $'Layout.alignment: (batteryIndicator.isReady) ? (Qt.AlignRight | Qt.AlignVCenter) : Qt.AlignVCenter' $'Layout.alignment: Qt.AlignHCenter | Qt.AlignVCenter' \
-          --replace-fail $'        Item {\n          Layout.preferredWidth: Style.marginM\n        }\n      }\n\n      // Password input' $'        Item {\n          Layout.fillWidth: true\n        }\n      }\n\n      // Password input'
+          --replace-fail $'Layout.fillWidth: batteryIndicator.isReady' $'Layout.preferredWidth: Style.marginM' \
+          --replace-fail $'Layout.fillWidth: !(Settings.data.location.weatherEnabled && LocationService.data.weather !== null)' $'Layout.fillWidth: false' \
+          --replace-fail $'          Layout.preferredWidth: Style.marginM\n          visible: MediaService.currentPlayer && MediaService.canPlay\n' $'          Layout.fillWidth: true\n' \
+          --replace-fail $'        ColumnLayout {\n          Layout.alignment: (batteryIndicator.isReady) ? (Qt.AlignRight | Qt.AlignVCenter) : Qt.AlignVCenter\n          spacing: Style.marginM' $'        RowLayout {\n          Layout.alignment: Qt.AlignHCenter | Qt.AlignVCenter\n          spacing: Style.marginXL' \
+          --replace-fail $'        Item {\n          Layout.preferredWidth: Style.marginM\n        }\n      }\n\n      // Password input' $'        Item {\n          Layout.fillWidth: true\n        }\n      }\n\n      // Password input' \
+          --replace-fail $'visible: MediaService.currentPlayer && MediaService.canPlay' $'visible: true' \
+          --replace-fail $'visible: !(MediaService.currentPlayer && MediaService.canPlay)' $'visible: false' \
+          --replace-fail $'Layout.preferredWidth: 220' $'Layout.preferredWidth: 360' \
+          --replace-fail $'visible: keyboardLayout.currentLayout !== "Unknown"' $'visible: false' \
+          --replace-fail $'              NText {\n                text: MediaService.trackTitle || "No media"\n                pointSize: Style.fontSizeM\n                color: Color.mOnSurface\n                Layout.fillWidth: true\n                elide: Text.ElideRight\n              }' $'              NScrollText {\n                text: MediaService.trackTitle || "No media"\n                Layout.fillWidth: true\n                maxWidth: parent.width\n                scrollMode: NScrollText.ScrollMode.Always\n                delegate: NText {\n                  pointSize: Style.fontSizeM\n                  color: Color.mOnSurface\n                }\n              }' \
+          --replace-fail $'              NText {\n                text: MediaService.trackArtist || ""\n                pointSize: Style.fontSizeM\n                color: Color.mOnSurfaceVariant\n                Layout.fillWidth: true\n                elide: Text.ElideRight\n              }' $'              NScrollText {\n                text: MediaService.trackArtist || ""\n                Layout.fillWidth: true\n                maxWidth: parent.width\n                scrollMode: NScrollText.ScrollMode.Always\n                delegate: NText {\n                  pointSize: Style.fontSizeM\n                  color: Color.mOnSurfaceVariant\n                }\n              }'
       '';
     });
 
@@ -18,6 +29,11 @@
       lock_cmd = "noctalia-shell ipc call lockScreen lock";
       before_sleep_cmd = "noctalia-shell ipc call lockScreen lock";
     };
+  };
+
+  programs.noctalia-shell.settings.general = {
+    clockFormat = "hh\\nmm\\nAP";
+    enableLockScreenMediaControls = true;
   };
 
   programs.noctalia-shell.settings.idle = {
