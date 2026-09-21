b20cc848dd27d75ac136a9d38639edd70caf2599
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-21 02:27:58 -0500

    refactor(nvim): group keymaps by plugin feature

--- home-manager/programs/nvim/config/keymaps.nix
+++ home-manager/programs/nvim/config/keymaps.nix
@@ -29,8 +29,8 @@ let
     )
   ];
 
-  typst = [
-    (normal "<leader>tp" "<cmd>TypstPreviewToggle<CR>" "Toggle Typst preview")
+  gitbrowse = [
+    (normal "<leader>gB" "<cmd>lua Snacks.gitbrowse()<CR>" "Open file on GitHub")
   ];
 
   neogit = [
@@ -41,27 +41,25 @@ let
     (normal "-" "<cmd>Oil<CR>" "Browse parent directory")
   ];
 
-  snacks = [
+  picker = [
     (normal "<leader><space>" "<cmd>lua Snacks.picker.files()<CR>" "Find files")
     (normal "<leader>ff" "<cmd>lua Snacks.picker.files()<CR>" "Find files")
     (normal "<leader>/" "<cmd>lua Snacks.picker.grep()<CR>" "Search project")
     (normal "<leader>bb" "<cmd>lua Snacks.picker.buffers()<CR>" "Switch buffer")
     (normal "<leader>e" "<cmd>lua Snacks.picker.explorer()<CR>" "File explorer")
-
     (normal "gd" "<cmd>lua Snacks.picker.lsp_definitions()<CR>" "Go to definition")
     (normal "gD" "<cmd>lua Snacks.picker.lsp_declarations()<CR>" "Go to declaration")
     (normal "gr" "<cmd>lua Snacks.picker.lsp_references()<CR>" "References")
     (normal "gI" "<cmd>lua Snacks.picker.lsp_implementations()<CR>" "Go to implementation")
     (normal "gy" "<cmd>lua Snacks.picker.lsp_type_definitions()<CR>" "Go to type definition")
+  ];
+
+  scratch = [
     (normal "<leader>S" "<cmd>lua Snacks.scratch()<CR>" "Scratch buffer")
     (normal "<leader>Ss" "<cmd>lua Snacks.scratch.select()<CR>" "Select scratch buffer")
+  ];
 
-    (normal "<leader>gB" "<cmd>lua Snacks.gitbrowse()<CR>" "Open file on GitHub")
-
-    (normal "<leader>ts" "<cmd>lua Snacks.toggle.option('spell')<CR>" "Toggle spell")
-    (normal "<leader>tw" "<cmd>lua Snacks.toggle.option('wrap')<CR>" "Toggle wrap")
-    (normal "<leader>ti" "<cmd>lua Snacks.toggle.inlay_hints()<CR>" "Toggle inlay hints")
-
+  terminal = [
     {
       mode = [
         "n"
@@ -82,6 +80,12 @@ let
     }
   ];
 
+  toggle = [
+    (normal "<leader>ts" "<cmd>lua Snacks.toggle.option('spell')<CR>" "Toggle spell")
+    (normal "<leader>tw" "<cmd>lua Snacks.toggle.option('wrap')<CR>" "Toggle wrap")
+    (normal "<leader>ti" "<cmd>lua Snacks.toggle.inlay_hints()<CR>" "Toggle inlay hints")
+  ];
+
   trouble = [
     (normal "<leader>cd" "<cmd>Trouble diagnostics toggle filter.buf=0<CR>" "Buffer diagnostics")
     (normal "<leader>cs" "<cmd>Trouble symbols toggle focus=false<CR>" "Symbols")
@@ -91,7 +95,23 @@ let
     (normal "<leader>xq" "<cmd>Trouble qflist toggle<CR>" "Quickfix list")
     (normal "<leader>xl" "<cmd>Trouble loclist toggle<CR>" "Location list")
   ];
+
+  typst = [
+    (normal "<leader>tp" "<cmd>TypstPreviewToggle<CR>" "Toggle Typst preview")
+  ];
 in
 {
-  keymaps = claudecode ++ codecompanion ++ conform ++ typst ++ neogit ++ oil ++ snacks ++ trouble;
+  keymaps =
+    claudecode
+    ++ codecompanion
+    ++ conform
+    ++ gitbrowse
+    ++ neogit
+    ++ oil
+    ++ picker
+    ++ scratch
+    ++ terminal
+    ++ toggle
+    ++ trouble
+    ++ typst;
 }
