ebb94bdd5abb8fde94a568c92b77e4680dd682e7
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-21 02:06:36 -0500

    feat(nvim): add bufferline

---
+++ home-manager/programs/nvim/config/bufferline.nix
@@ -0,0 +1,7 @@
+{
+  plugins = {
+    bufferline.enable = true;
+    bufferline.settings.options.style_preset.__raw = "require('bufferline').style_preset.no_italic";
+    web-devicons.enable = true;
+  };
+}
--- home-manager/programs/nvim/config/default.nix
+++ home-manager/programs/nvim/config/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./bufferline.nix
     ./completion.nix
     ./editor.nix
     ./lsp.nix
