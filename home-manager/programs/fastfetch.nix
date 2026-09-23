2b95bf4a4fa65a3ca0e605e9b830153a3dc2e1ac
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-23 17:09:14 -0500

    generation 53 (26.11.20260922.6774f7b)

--- home-manager/programs/fastfetch.nix
+++ home-manager/programs/fastfetch.nix
@@ -60,7 +60,7 @@ in {
         }
         {
           type = "custom";
-          key = "│ {#31}󰇄 toolkit      {#keys}│";
+          key = "│ {#31}󰇄 toolkit {#keys}│";
           format = "noctalia ${config.programs.noctalia-shell.package.version}";
         }
         {
